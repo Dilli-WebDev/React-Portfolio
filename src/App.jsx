18d800a71aee9730845efac98b6bacbd38dbb12c
Dilli_Rao <dilliwebdev218@gmail.com>
2025-04-18 20:17:23 +0530

    Added Demo Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1>React Portfolio - Coding in Progress</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+    </>
+  );
 }
-
+s;
 export default App;
