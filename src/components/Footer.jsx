68bbf76c112bf532f2f5b245fdabf16a44534bd3
Dilli_Rao <dilliwebdev218@gmail.com>
2025-07-07 16:15:23 +0530

    Fresh start

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center py-6 text-gray-500 text-sm bg-background">
-      © {new Date().getFullYear()} Dilli Rao. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
